5c56c1d468e4f97728eacddc3903f2d9747cdecf
Boaz Sender <boaz@bocoup.com>
2012-06-29 19:55:24 -0400

    implenmenting buttons

--- js/cravasse.js
+++ js/cravasse.js
@@ -78,8 +78,9 @@ $(function(){
     spriteY: 0
   });
 
+  var force = 600;
+
   player.onKeydown(function( e ){
-    var force = 600;
     
     // Right
     if (e.keyCode === 39) {
@@ -104,6 +105,29 @@ $(function(){
         inputState.up = true;
       }
     }
+  });
+
+  // var event = document.createEvent("KeyboardEvent");
+  // event.keyCode = 39;
+  // event.initEvent('keydown', true, true);
+  // window.dispatchEvent(event)
+
+  $("#right").on('click', function(){
+    player.setForce('movement', force, 90);
+    inputState.right = true;
+    player.lastDirection = 'right';
+  });
+
+  $("#jump").on('click', function(){
+    player.jumps++
+    player.applyImpulse( 50 );
+    inputState.up = true;
+  });
+
+  $("#left").on('click', function(){
+    player.setForce('movement', force, 270);
+    inputState.left = true;
+    player.lastDirection = 'left';
   });
 
   player.onKeyup(function( e ){
