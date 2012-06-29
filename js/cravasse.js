5ab668500f94c7dfea7f8e79e25267b91ccba64b
Boaz Sender <boaz@bocoup.com>
2012-06-29 19:26:40 -0400

    fixes camera on floor issue

--- js/cravasse.js
+++ js/cravasse.js
@@ -171,7 +171,7 @@ $(function(){
   world.onRender(function(ctx) {
     var p = player.position();
     var c = this.camera();
-    if( p.y < 0 ) {
+    if( p.y < .4 ) {
       this.camera({x: 0, y: p.y - 7 });
     }
     
