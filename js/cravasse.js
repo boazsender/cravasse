7ed8fad822b14f98d36c511f9f7c03fd242c81da
Boaz Sender <boaz@bocoup.com>
2012-07-02 17:54:36 -0400

    comments out keyboard code

--- js/cravasse.js
+++ js/cravasse.js
@@ -185,23 +185,23 @@ $(function(){
     // event.initEvent('keydown', true, true);
     // window.dispatchEvent(event)
 
-    $("#right").on('click', function(){
-      player.setForce('movement', force, 90);
-      inputState.right = true;
-      player.lastDirection = 'right';
-    });
-
-    $("#jump").on('click', function(){
-      player.jumps++;
-      player.applyImpulse( 50 );
-      inputState.up = true;
-    });
-
-    $("#left").on('click', function(){
-      player.setForce('movement', force, 270);
-      inputState.left = true;
-      player.lastDirection = 'left';
-    });
+    // $("#right").on('click', function(){
+    //   player.setForce('movement', force, 90);
+    //   inputState.right = true;
+    //   player.lastDirection = 'right';
+    // });
+    // 
+    // $("#jump").on('click', function(){
+    //   player.jumps++;
+    //   player.applyImpulse( 50 );
+    //   inputState.up = true;
+    // });
+    // 
+    // $("#left").on('click', function(){
+    //   player.setForce('movement', force, 270);
+    //   inputState.left = true;
+    //   player.lastDirection = 'left';
+    // });
 
     player.onKeyup(function( e ){
       player.clearForce( 'movement' );
