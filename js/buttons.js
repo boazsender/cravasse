9aea560511595bf834b30a22b6bd17e65b17dc7d
Boaz Sender <boaz@bocoup.com>
2012-07-06 14:32:47 -0400

    move errant button code to button.js

---
+++ js/buttons.js
@@ -0,0 +1,23 @@
+// var event = document.createEvent("KeyboardEvent");
+// event.keyCode = 39;
+// event.initEvent('keydown', true, true);
+// window.dispatchEvent(event)
+
+// $("#right").on('click', function(){
+//   player.setForce('movement', force, 90);
+//   inputState.right = true;
+//   player.lastDirection = 'right';
+// });
+// 
+// $("#jump").on('click', function(){
+//   player.jumps++;
+//   player.applyImpulse( 50 );
+//   inputState.up = true;
+// });
+// 
+// $("#left").on('click', function(){
+//   player.setForce('movement', force, 270);
+//   inputState.left = true;
+//   player.lastDirection = 'left';
+// });
+
